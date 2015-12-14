4a463d7992ed877822c598b06fc276060ff3c875
hisamrw <xikyu46@rwdev.site>
2015-12-14 11:09:37 +0700

    home trim content

--- templates/surakarta/modules/mod_news/index_view.php
+++ templates/surakarta/modules/mod_news/index_view.php
@@ -1,3 +1,4 @@
+<? $lang_sess = (getlang()) ? '_'.getlang() : ''?> 
 <div class="col-md-5">
 				<h3 class="home-title">News</h3>
 					<div class="home-post-list">
@@ -6,8 +7,8 @@
 								<div class="home-post-content">
 									<span class="home-post-date text-muted"><?= date('F d,Y',$row->update);?></span>
 									<img class="img-responsive img-thumbnail" src="<?= $row->image;?>">
-									<h4 class="home-post-title"><a href="#"><?= $row->title;?></a></h4>
-									<p><?= $row->content;?></p>
+									<h4 class="home-post-title"><a href="#"><?= $row->{'title'.$lang_sess};?></a></h4>
+									<p><?= content_trim($row->{'content'.$lang_sess},200);?></p>
 									<hr>
 								</div>		
 							<? }?>
