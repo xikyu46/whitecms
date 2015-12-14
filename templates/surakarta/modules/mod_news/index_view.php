e17257f85877c7b5dd9f9d28c1dfa6b576342010
hisamrw <xikyu46@rwdev.site>
2015-12-14 11:10:46 +0700

    add url di news view

--- templates/surakarta/modules/mod_news/index_view.php
+++ templates/surakarta/modules/mod_news/index_view.php
@@ -7,7 +7,7 @@
 								<div class="home-post-content">
 									<span class="home-post-date text-muted"><?= date('F d,Y',$row->update);?></span>
 									<img class="img-responsive img-thumbnail" src="<?= $row->image;?>">
-									<h4 class="home-post-title"><a href="#"><?= $row->{'title'.$lang_sess};?></a></h4>
+									<h4 class="home-post-title"><a href="<?= base_url($row->url);?>"><?= $row->{'title'.$lang_sess};?></a></h4>
 									<p><?= content_trim($row->{'content'.$lang_sess},200);?></p>
 									<hr>
 								</div>		
