db9462dd6a68f6fe5289bf7b3b4763ffe773d6f2
hisamrw <xikyu46@rwdev.site>
2015-12-19 06:34:42 +0700

    microsite update

--- application/modules/microsite/controllers/Microsite.php
+++ application/modules/microsite/controllers/Microsite.php
@@ -3,14 +3,16 @@
 Class Microsite extends MX_Controller{
 
 	function index($url=false){
-		$microsite = db_read('mod_microsite',array('url' => $url));
-		if($microsite){
-			$view['microsite'] = $microsite;
-			$view['template'] = db_read('mod_microsite_template',array('mid',$microsite->id));
-			$view['contact'] = db_read('mod_microsite_contact',array('mid',$microsite->id));
-			$view['content'] = db_reads('mod_microsite_content',array('mid',$microsite->id));
-			$this->load->view(tpldir('page/microsite'),$view);
-			return true;
+		if($url){
+			$microsite = db_read('mod_microsite',array('url' => $url));
+			if($microsite){
+				$view['microsite'] = $microsite;
+				$view['template'] = db_read('mod_microsite_template',array('mid',$microsite->id));
+				$view['contact'] = db_read('mod_microsite_contact',array('mid',$microsite->id));
+				$view['content'] = db_reads('mod_microsite_content',array('mid',$microsite->id));
+				$this->load->view(tpldir('page/microsite'),$view);
+				return true;
+			}
 		}
 		return false;
 	}
