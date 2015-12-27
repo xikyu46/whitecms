201b12bf0a6420cd29d1b48250dac91db45ae11d
hisamrw <xikyu46@rwdev.site>
2015-12-27 22:44:27 +0700

    halaman event

---
+++ application/logs/log-2015-12-27.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-12-27 22:43:39 --> 404 Page Not Found: 
+ERROR - 2015-12-27 22:43:44 --> 404 Page Not Found: 
+ERROR - 2015-12-27 22:43:49 --> 404 Page Not Found: 
--- templates/surakarta/modules/mod_event/index_view.php
+++ templates/surakarta/modules/mod_event/index_view.php
@@ -8,38 +8,10 @@
 					<span class="text-muted"><?= date('d F Y',$row->date);?></span>
 				</div>
 				<h4 class="event-title">
-					<a href="#"><?= $row->title;?></a>
+					<a href="<?= base_url($row->url);?>"><?= $row->title;?></a>
 				</h4>
 			</div>
 		<? }?>
 	<? }?>
-      
-      <div class="col-md-3">
-        <div class="event-date">
-          <i class="fa fa-calendar"></i>
-          <span class="text-muted">25 November 2015</span>
-        </div>
-        <h4 class="event-title">
-          <a href="#">Lorem Ipsum is simply dummy text.</a>
-        </h4>
-      </div>
-      <div class="col-md-3">
-        <div class="event-date">
-          <i class="fa fa-calendar"></i>
-          <span class="text-muted">25 November 2015</span>
-        </div>
-        <h4 class="event-title">
-          <a href="#">Lorem Ipsum is simply dummy text.</a>
-        </h4>
-      </div>
-      <div class="col-md-3">
-        <div class="event-date">
-          <i class="fa fa-calendar"></i>
-          <span class="text-muted">25 November 2015</span>
-        </div>
-        <h4 class="event-title">
-          <a href="#">Lorem Ipsum is simply dummy text.</a>
-        </h4>
-      </div>
     </div>
    
