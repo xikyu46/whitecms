6a4446369e42a5f5958838b22ca53959e6dfdee0
hisamrw <xikyu46@rwdev.site>
2016-01-15 16:53:23 +0700

    perbaikan event

--- templates/surakarta/modules/mod_event/index_view.php
+++ templates/surakarta/modules/mod_event/index_view.php
@@ -3,18 +3,12 @@
 	<? if($event){?>
 		<? for($i = count($event)-1; $i >= 0 ; $i--){?>
 			<div class="col-md-3">
-				<div class="event-date">
+				<div class="event-date kuning">
 					<i class="fa fa-calendar"></i>
-					<span class="text-muted"><?= date('d F Y',$event[$i]->date);?></span>
+					<span class="home-event-date text-muted ijo"><?= date('d F Y',$event[$i]->date);?></span>
 				</div>
-				<h4 class="event-title">
-					<? if(preg_match('#'.preg_quote('http://').'#',$event[$i]->url)){?>
-						<a href="<?= $event[$i]->url;?>"><?= $event[$i]->title;?></a>
-					<? }else{?>
-						<a href="<?= base_url($event[$i]->url);?>"><?= $event[$i]->title;?></a>
-					<? }?>
-					
-				</h4>
+				<img class="img-responsive img-thumbnail" src="<?= $event[$i]->image;?>">
+				<div class="event-title"><a href="<?= $event[$i]->url;?>"><?= $event[$i]->title;?></a></div>
 			</div>
 		<? }?>
 	<? }?>
