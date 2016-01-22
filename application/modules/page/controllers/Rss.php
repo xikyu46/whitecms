ce9e8986e215c701174039260b8f8daf674aa7f0
hisamrw <xikyu46@rwdev.site>
2016-01-22 13:01:34 +0700

    update rss

--- application/modules/page/controllers/Rss.php
+++ application/modules/page/controllers/Rss.php
@@ -4,8 +4,30 @@ Class Rss extends MX_Controller{
 	function index(){
 		$this->load->helper('xml');
 		$order['date'] = 'desc';
+		$configurationsResult = db_reads('configurations');
+		$config = false;
+		if($configurationsResult){
+			foreach($configurationsResult as  $row){
+				$config[$row->name] = $row->value;
+			}
+		}
+		
 		$news = db_reads('page',array('type' => 'news', 'status' => 1),$order);
 		
-		toXML($news);
+		$xml['channel'] = false;
+		$xml['title'] = (isset($config['websitename'])) ? $config['websitename'] : false;
+		$xml['link'] = base_url();
+		$xml['item'] = false;
+		if($news){
+			foreach($news as $newsrow){
+				$temp=false;
+				$temp['title'] = $newsrow->title;
+				$temp['link'] = base_url($newsrow->url);
+				$temp['description'] = $newsrow->content;
+				$xml['item'][] = $temp;
+			}
+		}
+		header("Content-Type: text/xml");
+		echo toXML($xml, 'rss version="2.0"');
 	}
 }
